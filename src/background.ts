e04400cbd8de32698cfcbf8a6bbec5fd4d70932a
Ivan Demchenko <ivan.demchenko@personio.de>
2021-10-12 09:18:58 +0200

    handle multiple languages

--- src/background.ts
+++ src/background.ts
@@ -16,23 +16,63 @@ const sendTabMessage = <T>(
   });
 };
 
+const languagesMap = {
+  BG: "Bulgarian",
+  CS: "Czech",
+  DA: "Danish",
+  DE: "German",
+  EL: "Greek",
+  "EN-GB": "English (British)",
+  "EN-US": "English (American)",
+  ES: "Spanish",
+  ET: "Estonian",
+  FI: "Finnish",
+  FR: "French",
+  HU: "Hungarian",
+  IT: "Italian",
+  JA: "Japanese",
+  LT: "Lithuanian",
+  LV: "Latvian",
+  NL: "Dutch",
+  PL: "Polish",
+  "PT-PT": "Portuguese (excluding Brazilian Portuguese)",
+  "PT-BR": "Portuguese (Brazilian)",
+  RO: "Romanian",
+  RU: "Russian",
+  SK: "Slovak",
+  SL: "Slovenian",
+  SV: "Swedish",
+  ZH: "Chinese",
+};
+
 chrome.runtime.onInstalled.addListener(() => {
-  chrome.contextMenus.create({
+  const rootMenuItem = chrome.contextMenus.create({
     id: "chrome-deepl",
-    title: "Translate with Deepl",
-    type: "normal",
-    contexts: ["selection"],
+    title: "Translate with Deepl to...",
+    contexts: ["editable"],
   });
 
+  for (let key in languagesMap) {
+    chrome.contextMenus.create({
+      id: `chrome-deepl-${key}`,
+      title: languagesMap[key],
+      parentId: rootMenuItem,
+      contexts: ["editable"],
+    });
+  }
+
   chrome.contextMenus.onClicked.addListener(async (info, tab) => {
-    if (info.menuItemId === "chrome-deepl") {
+    if (info.parentMenuItemId && info.parentMenuItemId === "chrome-deepl") {
+      const targetLang = (info.menuItemId as string).substr(
+        "chrome-deepl-".length
+      );
       const clickedEl = await sendTabMessage<GetClickedElementMsgResponse>(
         { type: "get-clicked-element" },
         info.frameId,
         tab
       );
       const rawResp = await fetch(
-        `https://api-free.deepl.com/v2/translate?auth_key=${process.env.APIKEY}&text=${clickedEl.value}&target_lang=de`
+        `https://api-free.deepl.com/v2/translate?auth_key=${process.env.APIKEY}&text=${clickedEl.value}&target_lang=${targetLang}`
       );
       const deeplResp = await rawResp.json();
       await sendTabMessage<SetClickedElementValueMsgResponse>(
